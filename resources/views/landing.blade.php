95e0f0be204c39b37cd28c72a2ee39e6de3d874d
We <wemonadevi@gmail.com>
2025-03-19 23:49:38 +0700

    Buat Landing Page

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WargaNet - Platform Warga Digital</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+<div class="background"></div>
+    <div class="navbar">
+        <div class="logo">Warga<span style="color:#3498db">Net</span></div>
+        <div>
+            <a href="#">Home</a>
+            <a href="/login" class="btn">Login</a>
+        </div>
+    </div>
+
+    <div class="hero">
+        <h1>Warga<span style="color:#3498db">Net</span></h1>
+        <h2 style="color:#1f57c3">Satu Platform Digital, Satu Komunitas yang Terhubung</h2>
+        <h2 style="color:#3498db">Selamat Datang Warga Desa Konoha!</h2>
+        <p>Kini, urusan warga jadi lebih mudah, cepat, dan transparan.
+        Mulai dari informasi penting, pembayaran iuran, hingga diskusi warga—semua terintegrasi dalam satu sistem!</p>
+
+        <div class="buttons">
+            <a href="/login" class="btn">Masuk</a>
+            <a href="/register" class="btn">Daftar</a>
+        </div>
+
+        <p style="margin-top: 30px; font-weight: 500;">Masuk sebagai Admin:</p>
+        <a href="/admin" class="btn-admin">Admin</a>
+
+    </div>
+</div> 
+
+    <div class="footer">
+        Copyright © 2025 WargaNet
+    </div>
+</body>
+
+</html>
