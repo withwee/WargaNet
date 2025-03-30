d9344b31bc40f5e2fb070e535b1f2a24c4e5be13
farhannoorsaid <farhannoorsaid83@gmail.com>
2025-03-30 18:27:27 +0700

    tes

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Warganet Dashboard</title>
+</head>
+<body>
+@extends('layouts.app')
+
+@section('content')
+  
+<main>
+  <h1>selamat datang</h1>
+</main>
+
+@endsection
+</body>
+</html>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Authentication')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @yield('styles')
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+</head>
+<body>
+    <main class="h-screen w-full flex  items-center justify-end"
+    style="background-image: linear-gradient(rgba(44, 121, 255, 0.35), rgba(44, 121, 255, 0.35)), url('images/Background.png'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+    <div class="w-full h-screen  relative flex items-center justify-end">
+    <div class="flex flex-col relative items-center gap-4">
+    <a href="/login">
+        @if (Request::is('login'))
+            <button class="border w-40 py-3 font-bold bg-white text-blue-500 rounded-bl-3xl rounded-tl-3xl transition">
+                Masuk
+            </button>
+        @else
+            <span class="font-bold text-black">Masuk</span>
+        @endif
+    </a>
+
+    <a href="/register">
+        @if (Request::is('register'))
+            <button class="border w-40 py-3 font-bold bg-white text-blue-500  rounded-bl-3xl rounded-tl-3xl transition">
+                Daftar
+            </button>
+        @else
+            <span class="font-bold text-black">Daftar</span>
+        @endif
+    </a>
+</div>
+
+        <p class="absolute bottom-10 right-56 text-white">Copyright ©{{ date('Y') }} WargaNet</p>
+    </div>
+
+    <div class="bg-white h-screen items-center justify-center flex flex-col w-full">
+
+<div class="w-96">
+      
+    <div class="flex items-center justify-center gap-0.5 text-5xl text-[#2C79FF]">
+        <h1 class="font-extrabold">Warga</h1>
+        <h1>Net</h1>
+    </div>
+
+    @yield('content')
+</div>
+</div>
+    </main>
+</body>
+</html>
