c2d5ffed132b51fd33282ccb0ce781cedaa3b1be
dimasarkaan <dimasarkaan11@gmail.com>
2025-04-20 02:14:06 +0700

    Fix Iuran Route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengumumanController;
+use App\Http\Controllers\IuranController;
 
 // Public routes hanya bisa diakses jika belum login
 Route::middleware('redirect.custom')->group(function () {
@@ -21,13 +22,9 @@ Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 // Route setelah login
 Route::middleware('auth.custom')->group(function () {
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
-    //Route::get('/pengumuman', [UserController::class, 'pengumuman'])->name('pengumuman');
-    Route::resource('pengumuman', PengumumanController::class)->names([
+        Route::resource('pengumuman', PengumumanController::class)->names([
         'index' => 'pengumuman',
     ]);
-    // kalo page nya udah selesai user con ganti pengumuman controller
-    // get nya ganti resource 
-    //Route::resource('pengumuman', PengumumanController::class);
     Route::get('/forum', [UserController::class, 'forum'])->name('forum');
     Route::get('/kalender', [UserController::class, 'kalender'])->name('kalender');
     
